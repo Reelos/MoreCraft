9ff67453c194c8ed90f404de2a62897987c34040
herrlock <herrlock@t-online.de>
2016-11-01 19:25:16 +0200

    add custom exception for the case that a file cannot be found; remove unused method

--- src/main/java/de/reelos/recipecreator/RecipeCreator.java
+++ src/main/java/de/reelos/recipecreator/RecipeCreator.java
@@ -1,17 +1,30 @@
 package de.reelos.recipecreator;
 
+import java.util.Set;
 import java.util.logging.Level;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.google.common.collect.ImmutableSet;
+
+import de.reelos.recipecreator.util.CannotReadJsonException;
 import de.reelos.recipecreator.util.RecipeReader;
 
 public class RecipeCreator extends JavaPlugin {
+
+    private final Set<String> jsons = ImmutableSet.of( "dirt", "lava" );
+
+    @SuppressWarnings( "unused" )
     @Override
     public void onEnable() {
         getLogger().log( Level.INFO, "RecipeCreator loaded." );
-        new RecipeReader( "./misc/dirt.json" );
-        new RecipeReader( "./misc/lava.json" );
+        for ( String string : this.jsons ) {
+            try {
+                new RecipeReader( "./misc/" + string + ".json" );
+            } catch ( CannotReadJsonException ex ) {
+                getLogger().log( Level.WARNING, "Could not load " + string, ex );
+            }
+        }
     }
 
     @Override
