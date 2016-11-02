041e011ca6f7283be02a5374260e5d7d01ac0692
André Köhler <reelos.hericon@web.de>
2016-11-02 19:54:59 +0100

    added recipes folder. onEnable now itarates recipes folder

--- src/main/java/de/reelos/recipecreator/RecipeCreator.java
+++ src/main/java/de/reelos/recipecreator/RecipeCreator.java
@@ -1,34 +1,50 @@
 package de.reelos.recipecreator;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.google.common.collect.ImmutableSet;
 
 import de.reelos.recipecreator.util.CannotParseJsonException;
 import de.reelos.recipecreator.util.RecipeReader;
 
 public class RecipeCreator extends JavaPlugin {
 
-    private final Set<String> jsons = ImmutableSet.of( "dirt", "lava" );
+	private final Path recipeFolder = Paths.get("./recipes/");
 
-    @Override
-    public void onEnable() {
-        getLogger().log( Level.INFO, "RecipeCreator loaded." );
-        for ( String string : this.jsons ) {
-            try {
-                new RecipeReader( "./misc/" + string + ".json" ).registerRecipe();
-            } catch ( FileNotFoundException | CannotParseJsonException ex ) {
-                getLogger().log( Level.WARNING, "Could not load " + string, ex );
-            }
-        }
-    }
+	@Override
+	public void onEnable() {
+		getLogger().log(Level.INFO, "RecipeCreator loaded.");
 
-    @Override
-    public void onDisable() {
-        getLogger().log( Level.INFO, "RecipeCreator unloaded." );
-    }
+		if (!Files.exists(recipeFolder)) {
+			try {
+				Files.createDirectories(recipeFolder);
+			} catch (IOException e) {
+				getLogger().log(Level.WARNING, "Could not create 'recipeFolder'.", e);
+			}
+		}
+		for (String string : recipeFolder.toFile().list((File f, String s) -> f.getName().endsWith(".json"))) {
+			try {
+				new RecipeReader("./recipes/" + string + ".json").registerRecipe();
+			} catch (FileNotFoundException | CannotParseJsonException ex) {
+				getLogger().log(Level.WARNING, "Could not load " + string, ex);
+			}
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().log(Level.INFO, "RecipeCreator unloaded.");
+	}
+
+	public static void main(String[] args) {
+		for (String file : Paths.get("./recipes/").toFile().list()) {
+			System.out.println(file);
+		}
+	}
 }
