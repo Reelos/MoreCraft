15b49aa60a39b1293cdc54274a81c25343270b87
herrlock <herrlock@t-online.de>
2016-11-06 15:48:45 +0100

    return default amount of 1 if the amount is not set

--- src/main/java/de/reelos/recipecreator/entity/MoreRecipe.java
+++ src/main/java/de/reelos/recipecreator/entity/MoreRecipe.java
@@ -9,68 +9,68 @@ import com.google.gson.annotations.SerializedName;
  */
 public class MoreRecipe {
 
-    private String type;
-    @SerializedName( "for" )
-    private MoreRecipeFor _for;
-    private Collection<String> recipe;
-    private Collection<MoreRecipeIngredients> ingredients;
+	private String type;
+	@SerializedName("for")
+	private MoreRecipeFor _for;
+	private Collection<String> recipe;
+	private Collection<MoreRecipeIngredients> ingredients;
 
-    public String getType() {
-        return this.type;
-    }
+	public String getType() {
+		return this.type;
+	}
 
-    public MoreRecipeFor getFor() {
-        return this._for;
-    }
+	public MoreRecipeFor getFor() {
+		return this._for;
+	}
 
-    public Collection<String> getRecipe() {
-        return this.recipe;
-    }
+	public Collection<String> getRecipe() {
+		return this.recipe;
+	}
 
-    public Collection<MoreRecipeIngredients> getIngredients() {
-        return this.ingredients;
-    }
+	public Collection<MoreRecipeIngredients> getIngredients() {
+		return this.ingredients;
+	}
 
-    public static class MoreRecipeFor {
-        private String name, displayName;
-        private int amount;
-        private byte meta;
+	public static class MoreRecipeFor {
+		private String name, displayName;
+		private int amount;
+		private byte meta;
 
-        public String getName() {
-            return this.name;
-        }
+		public String getName() {
+			return this.name;
+		}
 
-        public String getDisplayName() {
-            return this.displayName;
-        }
+		public String getDisplayName() {
+			return this.displayName;
+		}
 
-        public int getAmount() {
-            return this.amount;
-        }
+		public int getAmount() {
+			return this.amount <= 0 ? 1 : this.amount;
+		}
 
-        public byte getMeta() {
-            return this.meta;
-        }
+		public byte getMeta() {
+			return this.meta;
+		}
 
-    }
+	}
 
-    public static class MoreRecipeIngredients {
-        private String name;
-        private char tag;
-        private int amount;
+	public static class MoreRecipeIngredients {
+		private String name;
+		private char tag;
+		private int amount;
 
-        public String getName() {
-            return this.name;
-        }
+		public String getName() {
+			return this.name;
+		}
 
-        public char getTag() {
-            return this.tag;
-        }
+		public char getTag() {
+			return this.tag;
+		}
 
-        public int getAmount() {
-            return this.amount;
-        }
+		public int getAmount() {
+			return this.amount;
+		}
 
-    }
+	}
 
 }
